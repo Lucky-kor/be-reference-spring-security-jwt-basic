a61eb46ded4056b01f23bae840f52b8495195eea
Jungmin Lee <75019459+Lucky-kor@users.noreply.github.com>
2024-07-09 14:24:07 +0900

    Add files via upload

---
+++ src/main/java/com/springboot/helper/event/MemberRegistrationApplicationEvent.java
@@ -0,0 +1,12 @@
+package com.springboot.helper.event;
+
+import com.springboot.member.entity.Member;
+import lombok.Getter;
+
+@Getter
+public class MemberRegistrationApplicationEvent {
+    private Member member;
+    public MemberRegistrationApplicationEvent(Member member) {
+        this.member = member;
+    }
+}
